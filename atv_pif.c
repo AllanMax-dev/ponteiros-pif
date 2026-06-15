ebd199148e03df3837e77f4ebc239e721e80af06
AllanMax-dev <allanjlima4@gmail.com>
2026-06-15 11:00:04 -0300

    feat: adiciona atividade 3 com struct produto

--- atv_pif.c
+++ atv_pif.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
-void aplicar_desconto(float *preco, float percentual_desconto) {
-    *preco = *preco - (*preco * percentual_desconto / 100);
+typedef struct {
+    int id;
+    float preco;
+} Produto;
+
+void aplicar_desconto(Produto *produto, float percentual_desconto) {
+    produto->preco = produto->preco - (produto->preco * percentual_desconto / 100);
 }
 
 int main() {
-    float preco = 100.0;
+    Produto produto = {1, 100.0};
 
-    printf("Preco antes do desconto: %.2f\n", preco);
+    printf("Produto %d antes do desconto: %.2f\n", produto.id, produto.preco);
 
-    aplicar_desconto(&preco, 10.0);
+    aplicar_desconto(&produto, 10.0);
 
-    printf("Preco depois do desconto: %.2f\n", preco);
+    printf("Produto %d depois do desconto: %.2f\n", produto.id, produto.preco);
 
     return 0;
 }
