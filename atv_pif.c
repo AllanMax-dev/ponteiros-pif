8c70da1f820aa9fbfa85071bc046f1416ecf4828
AllanMax-dev <allanjlima4@gmail.com>
2026-06-15 11:00:54 -0300

    feat: adiciona atividade 4 com array de produtos

--- atv_pif.c
+++ atv_pif.c
@@ -9,14 +9,31 @@ void aplicar_desconto(Produto *produto, float percentual_desconto) {
     produto->preco = produto->preco - (produto->preco * percentual_desconto / 100);
 }
 
+void imprimir_produtos(Produto *produtos, int quantidade) {
+    int i;
+
+    for (i = 0; i < quantidade; i++) {
+        printf("ID: %d | Preco: %.2f\n", (produtos + i)->id, (produtos + i)->preco);
+    }
+}
+
 int main() {
-    Produto produto = {1, 100.0};
+    Produto produtos[3] = {
+        {1, 100.0},
+        {2, 50.0},
+        {3, 250.0}
+    };
+    int i;
 
-    printf("Produto %d antes do desconto: %.2f\n", produto.id, produto.preco);
+    printf("Produtos antes do desconto:\n");
+    imprimir_produtos(produtos, 3);
 
-    aplicar_desconto(&produto, 10.0);
+    for (i = 0; i < 3; i++) {
+        aplicar_desconto(produtos + i, 10.0);
+    }
 
-    printf("Produto %d depois do desconto: %.2f\n", produto.id, produto.preco);
+    printf("\nProdutos depois do desconto:\n");
+    imprimir_produtos(produtos, 3);
 
     return 0;
 }
